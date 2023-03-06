79b06cb17fd971411e80aaa0ea6a3b37a8b80189
moeinh77 <moeinhasani77@gmail.com>
2023-03-05 18:17:14 -0600

    [FIX] Got the tokenizer to work

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,3 +1,6 @@
+import torch
+
+
 class SeqDataset_HF(torch.utils.data.Dataset):
     """
     This class is used to create a dataset for the HuggingFace transformers.
@@ -15,6 +18,7 @@ class SeqDataset_HF(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.labels)
 
+
 class SeqDataset(torch.utils.data.Dataset):
     """
     This class is used to create a dataset in format of pytorch datasets.
@@ -29,3 +33,28 @@ class SeqDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.labels)
+
+
+def return_kmer(seq, K=6):
+    """
+    This function outputs the K-mers of a sequence
+    
+    Parameters
+    ----------
+    seq : str
+        A single sequence to be split into K-mers
+    K : int, optional
+        The length of the K-mers, by default 6
+    
+    Returns
+    -------
+    kmer_seq : str
+        A string of K-mers separated by spaces
+    """
+    
+    kmer_list = []
+    for x in range(len(seq) - K + 1):
+        kmer_list.append(seq[x : x + K])
+
+    kmer_seq = " ".join(kmer_list)
+    return kmer_seq
